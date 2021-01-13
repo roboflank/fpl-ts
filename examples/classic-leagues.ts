c84f00baed74f213f27ac06392e71626a77bebb4
Denzel Wamburu <denzjoseph@gmail.com>
2021-01-13 13:53:53 +0300

    doc: add examples

---
+++ examples/classic-leagues.ts
@@ -0,0 +1,21 @@
+import { ClassicLeague } from 'fpl-ts'
+const league = new ClassicLeague(592)
+
+const getLeagues = async () => {
+  try {
+    const details = await league.getDetails()
+    console.log(details)
+  } catch (err) {
+    console.error('error ->', err)
+  }
+}
+const getLeaguesStandings = async () => {
+  try {
+    const standings = await league.getStandings(1)
+    console.log(standings)
+  } catch (err) {
+    console.error('error ->', err.message)
+  }
+}
+getLeagues()
+getLeaguesStandings()
